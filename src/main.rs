06c3a07c29212b977018ab5389b88619d26df441
devishell <517585288@qq.com>
2020-11-02 11:49:03 +0800

    安装学习链接

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+
+    //安装rust：https://www.rust-lang.org/zh-CN/tools/install
+    //clion使用文档 https://www.jetbrains.com/help/clion/rust-support.html#install
+    //学习文档1：https://www.runoob.com/rust/rust-basic-syntax.html
+    //学习文档2：file:///Users/ting/.rustup/toolchains/stable-x86_64-apple-darwin/share/doc/rust/html/rust-by-example/hello/print/print_display.html
+}
